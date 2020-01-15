d5f2b54655477d5adff9f6d696da7a76ae027ef4
Aaron Pleitez <aaron.pleitez12@gmail.com>
2020-01-15 00:29:18 -0600

    Stretch

---
+++ client/src/components/SimpleCard.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+    display: "flex"
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
+
+export default function SimpleCard({ post }) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Get request
+        </Typography>
+        <Typography variant="body2" component="p">
+          {post.title}
+        </Typography>
+        <Typography variant="body2" component="p">
+          {post.contents}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
